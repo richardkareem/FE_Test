3f0dae85e9f923b660df38cc2f0fb32e20ace727
Richard Abdul Kareem <81224883+richardkareem@users.noreply.github.com>
2024-11-13 15:28:48 +0700

    Update page.js

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -4,7 +4,7 @@ import Link from 'next/link'
 import React from 'react'
 import IcLogo from "../../../public/icLogo.png"
 function LoginPage() {
-  
+  //miawmiawmiawmiaw
   return (
     <div className='flex justify-center items-center flex-col min-h-svh w-svw'>
       <div className='flex flex-col bg-surfaceVariant w-96 bg- p-4 gap-2 shadow-sm rounded-md'>
